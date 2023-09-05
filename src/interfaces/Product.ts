24431fffb7b4bc923b00f1fb2703b2ab7b507334
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-09-04 21:01:06 -0300

    added Products Page

---
+++ src/interfaces/Product.ts
@@ -0,0 +1,11 @@
+export interface Props {
+  id: string;
+  code: string;
+  title: string;
+  description: string;
+  category: string;
+  price: string;
+  stock: string;
+  status: boolean;
+  thumbnails: string[];
+}
